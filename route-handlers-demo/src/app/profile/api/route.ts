5c84f5eecec739bfdb5277fcc70f173906f8dca0
Ductran-05 <23520320@gm.uit.edu.vn>
2025-08-11 14:55:08 +0700

    video 43

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,4 +1,19 @@
+import { type NextRequest } from "next/server";
+import { headers, cookies } from "next/headers";
+export async function GET(request: NextRequest) {
+    const headersList = await headers();
+    console.log(headersList.get("Authorization"));
 
-export async function GET() {
-    return new Response("Profile ");
+    const theme = request.cookies.get("theme")
+    console.log(theme);
+    const cookiesStore = await cookies();
+    cookiesStore.set("resultsPerPage", "10")
+    return new Response("Profile ",
+        {
+            headers: {
+                "Content-Type": "application/json",
+                "set-cookie": "theme=dark",
+            },
+        }
+    )
 }
