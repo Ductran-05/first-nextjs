7889a41362bae4ae31609fdb84a0907b9adeead0
Ductran-05 <23520320@gm.uit.edu.vn>
2025-08-11 12:23:03 +0700

    video 40

--- route-handlers-demo/src/app/comments/[id]/route.ts
+++ route-handlers-demo/src/app/comments/[id]/route.ts
@@ -4,3 +4,19 @@ export async function GET(_request: Request, { params }: { params: { id: string 
     const comment = comments.find((comment) => comment.id === parseInt(id));
     return new Response(JSON.stringify(comment));
 }
+
+export async function PATCH(_request: Request, { params }: { params: { id: string } }) {
+    const { id } = params;
+    const body = await _request.json();
+    const { text } = body;
+    const index = comments.findIndex((comment) => comment.id === parseInt(id));
+    comments[index].text = text;
+    return new Response(JSON.stringify(comments[index]));
+}
+
+export async function DELETE(_request: Request, { params }: { params: { id: string } }) {
+    const { id } = params;
+    const index = comments.findIndex((comment) => comment.id === parseInt(id));
+    comments.splice(index, 1);
+    return new Response(JSON.stringify(comments));
+}
